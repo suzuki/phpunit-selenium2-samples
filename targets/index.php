a3e2a2e331048e95e7fea464030222a2e0e38e17
Norio Suzuki <norio.suzuki@gmail.com>
2013-03-05 15:56:25 +0900

    Add top level index.php

---
+++ targets/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHPUnit Selenium2 Samples</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen" />
+  </head>
+
+  <body>
+    <div class="container">
+      <h1>PHPUnit + Selenium2 Samples</h1>
+
+      <ul class="hoge">
+        <li><a href="./01/">Sample 01</a></li>
+      </ul>
+
+    </div>
+  </body>
+</html>
